91a2d86010bf476f8958e42af190535e500471e5
codyMalcolm <cody@codymalcolm.com>
2018-08-18 18:34:17 -0400

    Tagging feature completed

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,3 +1,16 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(', ').map{|t| t.downcase}.uniq
+    new_or_found_tags = tag_names.map{|n| Tag.find_or_create_by(name: n)}
+    self.tags = new_or_found_tags
+  end
+
+  def tag_list
+    self.tags.map {|t| t.name}.join(", ")
+  end
+
 end
