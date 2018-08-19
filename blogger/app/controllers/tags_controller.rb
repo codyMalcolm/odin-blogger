3eff5ee512a3484a107ef1861b5cc1886de0c16a
codyMalcolm <cody@codymalcolm.com>
2018-08-19 18:02:14 -0400

    Sorcery authentication complete

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -7,6 +7,7 @@ class TagsController < ApplicationController
     @tag = Tag.find(params[:id])
   end
 
+  before_action :require_login, only: [:destroy]
   def destroy
     @tag = Tag.find(params[:id])
     @tag.destroy
